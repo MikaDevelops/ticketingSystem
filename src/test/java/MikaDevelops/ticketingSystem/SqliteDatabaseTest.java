f2e059a7e98e9902fd4fd50c1bc2cc9fcdcf831b
MikaDevelops <mika.virtala@gmail.com>
2025-04-08 08:08:13 +0300

    test data to match expected data

--- src/test/java/MikaDevelops/ticketingSystem/SqliteDatabaseTest.java
+++ src/test/java/MikaDevelops/ticketingSystem/SqliteDatabaseTest.java
@@ -140,11 +140,11 @@ class SqliteDatabaseTest {
                 "INSERT INTO service_person(name) values ('Patrick Star'),('Mr. Krabs'),('Squidward Tentacles');",
                 "INSERT INTO category(name) values ('Workstation'),('Server'),('Deep fryer'),('Plankton');",
                 "INSERT INTO solution(description) values ('Coffee on keyboard dryed using hairdryer.'),('Powercord plugged to wall outlet'),('Year old fries removed.'),('Used magic.');",
-                "INSERT INTO incident(created_datetime, subject, description, notes, related_incidents, status_id, customer_id, priority_id) values"
-                    +"(15120, 'test subject', 'test description', 'test notes', '1,2,3,4',1,1,1),"
-                    +"(15121, 'test subject442', 'test description442', 'test note33s2', '2,3',2,2,2),"
-                    +"(15122, 'test subject443', 'test description443', 'test not33es3', '3',3,2,3),"
-                    +"(15123, 'test subject444', 'test description444', 'test not33es4', '1',2,1,3);",
+                "INSERT INTO incident(created_datetime, subject, description, notes, related_incidents, status_id, customer_id, priority_id, solution_id) values"
+                    +"(15120, 'test subject', 'test description', 'test notes', '1,2,3,4',1,1,1,1),"
+                    +"(15121, 'test subject442', 'test description442', 'test note33s2', '2,3',2,2,2,2),"
+                    +"(15122, 'test subject443', 'test description443', 'test not33es3', '3',3,2,3,3),"
+                    +"(15123, 'test subject444', 'test description444', 'test not33es4', '1',2,1,3,4);",
                 "INSERT INTO incident_service_person(person_id, incident_id) VALUES (1,1), (2,2), (1,2);"
         };
 
